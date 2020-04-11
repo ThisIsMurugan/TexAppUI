9a533b4121e71c2a56b2f53b8d4371dec094949d
Murugan P <murugan.p.ext@siemensgamesa.com>
2020-04-11 22:27:27 +0530

    Renamed the app-root and component name

--- src/BasicDetailsApp/Ledger/BasicDetailsApp.Ledger.component.spec.ts
+++ src/BasicDetailsApp/Ledger/BasicDetailsApp.Ledger.component.spec.ts
@@ -1,35 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { LedgerComponent } from './BasicDetailsApp.Ledger.component';
 
-describe('AppComponent', () => {
+describe('LedgerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        LedgerComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LedgerComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TexApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'Tex ????'`, () => {
+    const fixture = TestBed.createComponent(LedgerComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TexApp');
+    expect(app.title).toEqual('Tex ????');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LedgerComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('TexApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Tex ???? app is running!');
   });
 });
